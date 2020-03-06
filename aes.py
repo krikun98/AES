6c79b99cc225599f9c6f950ae67a5596ca11b8f3
krikun98 <krikun98@gmail.com>
2020-03-06 06:23:44 +0300

    initial commit

---
+++ aes.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+import galois_math
+
+field = galois_math.Galois()
+
+
+def hexprint(arr):
+    for el in arr:
+        print(hex(el)[2:], ' ', end='')
+    print()
+
+
+def circ_l_shift(a, n=1, k=8):
+    for i in range(n):
+        a <<= 1
+        if a & 1 << k:
+            a ^= 1 << k
+            a ^= 1
+    return a
+
+
+def s_box(num, mode=False):
+    def affine(n):
+        aff = 0x63
+        s = n ^ circ_l_shift(n, 1)
+        s ^= circ_l_shift(n, 2)
+        s ^= circ_l_shift(n, 3)
+        s ^= circ_l_shift(n, 4)
+        s ^= aff
+        return s
+
+    def inv_affine(n):
+        aff = 0x5
+        t = circ_l_shift(n, 1)
+        t ^= circ_l_shift(n, 3)
+        t ^= circ_l_shift(n, 6)
+        t ^= aff
+        return t
+
+    if mode == 0:
+        # Encryption
+        b = field.inv(num)
+        b = affine(b)
+    else:
+        # Decryption
+        b = inv_affine(num)
+        b = field.inv(b)
+    return b
+
+
+def shift_arr(arr, n=1, dir=False):
+    res = arr[:]
+    for i in range(n):
+        if dir == False:
+            temp = res[1:]
+            temp.append(res[0])
+        else:
+            temp = res[:-1]
+            temp.insert(0, res[-1])
+        res[:] = temp[:]
+    return res
+
+
+def shift_rows(state, mode=False):
+    rows = []
+    for r in range(4):
+        rows.append(state[r::4])
+        if mode == False:
+            # Encryption
+            rows[r] = rows[r][r:] + rows[r][:r]
+        else:
+            # Decryption
+            rows[r] = rows[r][4 - r:] + rows[r][:4 - r]
+    return [r[c] for c in range(4) for r in rows]
+
+
+def mix_columns(state, mode=False):
+    def mult(a, b):
+        return field.mult(a, b)
+
+    matrices = [[2, 3, 1, 1], [14, 11, 13, 9]]
+    m = matrices[mode]
+    res = []
+    for i in range(4):
+        col = state[i * 4:(i + 1) * 4]
+        temp = []
+        for j in range(len(col)):
+            temp.append(0)
+            for k in range(len(m)):
+                temp[j] ^= mult(col[k], m[k])
+            m = shift_arr(m, 1, True)
+        res.extend(temp)
+    return res
+
+
+def xor_arr(s1, s2):
+    return tuple(a ^ b for a, b in zip(s1, s2))
+
+
+def key_schedule(key, nb=4, nr=10):
+    def sub_word(word):
+        return (s_box(b) for b in word)
+
+    def rot_word(word):
+        return shift_arr(word)
+
+    rcon = (0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a)
+    expanded = []
+    expanded.extend(map(ord, key))
+    for i in range(nb, nb * (nr + 1)):
+        t = expanded[(i - 1) * 4:i * 4]
+        if i % nb == 0:
+            t = xor_arr(sub_word(rot_word(t)), (rcon[i // nb], 0, 0, 0))
+        elif nb > 6 and i % nb == 4:
+            t = sub_word(t)
+        expanded.extend(xor_arr(t, expanded[(i - nb) * 4:(i - nb + 1) * 4]))
+    return expanded
+
+
+def add_round_key(state, rkey):
+    for i, b in enumerate(rkey):
+        state[i] ^= b
+
+
+def encrypt(block, key, nb=4, nr=10):
+    n = nb * 4
+    state = list(map(ord, block))
+    keys = key_schedule(key)
+    add_round_key(state, keys[0:n])
+    for r in range(1, nr):
+        state = [s_box(a) for a in state]
+        state = shift_rows(state)
+        state = mix_columns(state)
+        k = keys[r * n:(r + 1) * n]
+        add_round_key(state, k)
+
+    state = [s_box(a) for a in state]
+    state = shift_rows(state)
+    add_round_key(state, keys[nr * n:])
+    return "".join(map(chr, state))
+
+
+def decrypt(block, key, nb=4, nr=10):
+    n = nb * 4
+    state = list(map(ord, block))
+    keys = key_schedule(key)
+    k = keys[nr * n:(nr + 1) * n]
+    add_round_key(state, k)
+    for r in range(nr - 1, 0, -1):
+        state = shift_rows(state, 1)
+        state = [s_box(a, 1) for a in state]
+        k = keys[r * n:(r + 1) * n]
+        add_round_key(state, k)
+        state = mix_columns(state, 1)
+
+    state = shift_rows(state, 1)
+    state = [s_box(a, 1) for a in state]
+    add_round_key(state, keys[0:n])
+    return "".join(map(chr, state))
+
+
+# arr = []
+# for i in range(4):
+#     arr.append([])
+#     for j in range(4):
+#         arr[i].append(i + j)
+# arr = [219, 19, 83, 69, 242, 10, 34, 92, 213, 213, 215, 214, 45, 38, 49, 76]
+# print(arr)
+# arr = mix_columns(arr)
+# print(arr)
+# print(list(map(ord,'qwertyuiopasdfgh')))
+key = 'zxcvbshfdysywirt'
+enc = encrypt('qwertyuiopasdfgh', key)
+print(enc)
+dec = decrypt(enc, key)
+print(dec)
+# print(exkey)
---
+++ galois_math.py
@@ -0,0 +1,155 @@
+class Galois:
+    def __init__(self, irp=int("100011011", 2)):
+        self.irp = irp
+        self.k = self.irp.bit_length() - 1
+
+    def __mon_init__(self):
+        self.r = 1 << self.k
+        rt = self.r ^ self.irp
+        self.r_sq = self.mult(rt, rt)
+
+    def __gf2divmod(self, avec, bvec):
+        na = avec[0].bit_length() or 1
+        nd = bvec[0].bit_length()
+        i = na - nd
+        q = 0
+        test = 1 << (na - 1)
+        while i >= 0:
+            if (avec[0] & test) != 0:
+                avec = [a ^ (d << i) for (a, d) in zip(avec, bvec)]
+                q |= (1 << i)
+            i -= 1
+            test >>= 1
+        r = avec
+        return q, r
+
+    def __blankinship(self, a, b):
+        arow = [a, 1, 0]
+        brow = [b, 0, 1]
+        while True:
+            (_, rrow) = self.__gf2divmod(arow, brow)
+            if rrow[0] == 0:
+                break
+            arow = brow
+            brow = rrow
+        return tuple(brow)
+
+    def mult(self, a, b):
+        irp_mult = self.irp ^ (1 << self.k)
+        p = 0
+        while a > 0 and b > 0:
+            c = 0
+            if b & 1:
+                p ^= a
+            b >>= 1
+            a <<= 1
+            if a & 1 << self.k:
+                c = 1
+                a -= 1 << self.k
+            if c & 1:
+                a ^= irp_mult
+        return p
+
+    def add(self, a, b):
+        return a ^ b
+
+    def dumb_exp(self, a, e):
+        c = 1
+        for i in range(e):
+            c = self.mult(c, a)
+        return c
+
+    def exp(self, a, e):
+        c = 1
+        r = a
+        while e > 0:
+            if e & 1:
+                c = self.mult(c, r)
+            e >>= 1
+            if e > 0:
+                r = self.mult(r, r)
+        return c
+
+    def exp_ltor(self, a, e):
+        c = 1
+        for i in range(self.k - 1, -1, -1):
+            c = self.mult(c, c)
+            if e & 1 << i:
+                c = self.mult(c, a)
+        return c
+
+    def inv(self, a):
+        _, u, _ = self.__blankinship(a, self.irp)
+        return u
+
+    def mon_redc(self, a):
+        _, irp_tick, r_inv = self.__blankinship(self.irp, self.r)
+        m = (self.mult((a & (self.r - 1)), irp_tick) & (self.r - 1))
+        t = self.mult((a ^ self.mult(m, self.irp)), r_inv)
+        if t > self.irp:
+            return t ^ self.irp
+        else:
+            return t
+
+    def dumb_mon_mult(self, a, b):
+        t = self.mult(a, b)
+        return self.mon_redc(t)
+
+    def mon_mult(self, a, b):
+        c = 0
+        for i in range(self.k):
+            if (1 << i) & a:
+                c = c ^ b
+            if 1 & c:
+                c = c ^ self.irp
+            c >>= 1
+        return c
+
+    def montify(self, a):
+        return self.mon_mult(a, self.r_sq)
+
+    def mon_square(self, a):
+        c = 0
+        for i in range(self.k):
+            c += (a & 1) << 2 * i
+            a >>= 1
+        for i in range(self.k):
+            if c & 1:
+                c ^= self.irp
+            c >>= 1
+        return c
+
+    def mon_exp(self, a, e):
+        self.__mon_init__()
+        c = self.montify(1)
+        m = self.montify(a)
+        for i in range(self.k):
+            if e & (1 << i):
+                c = self.mon_mult(c, m)
+            m = self.mon_square(m)
+        return self.mon_mult(c, 1)
+
+    def mon_mult_and_square(self, m, c):
+        t = m
+        mul = 0
+        sqr = 0
+        for i in range(self.k - 1, -1, -1):
+            if t & 1:
+                t = t ^ self.irp
+            t >>= 1
+            if c & (1 << i):
+                mul ^= t
+            if m & (1 << i):
+                sqr ^= t
+        return sqr, mul
+
+    def mon_exp_kor(self, a, e):
+        self.__mon_init__()
+        c = self.montify(1)
+        m = self.montify(a)
+        for i in range(self.k):
+            if e & (1 << i):
+                m, c = self.mon_mult_and_square(m, c)
+            else:
+                m = self.mon_square(m)
+        return self.mon_redc(c)
